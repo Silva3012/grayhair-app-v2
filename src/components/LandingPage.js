35fc337eef64da92e0db13e34e0530352f29a134
Silva3012 <silvano.n92@gmail.com>
2023-03-30 02:04:43 +0200

    Added a carousel on the landing page  using boostrap

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/LandingPage.css';
+import jean_1 from '../images/Jean_1.jpeg';
+import plain_t from '../images/t_shirt_1.png';
+import sneakers from '../images/sneakers_js.png';
 
 function LandingPage() {
     return (
@@ -8,6 +13,29 @@ function LandingPage() {
             <h2>Welcome to GRAYHAIR Clothing</h2>
             {/* Description */}
             <p>We are a fashion-forward company that specializes in high quality clothing.</p>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={jean_1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={plain_t}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={sneakers}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 }
